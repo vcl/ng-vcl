9c043583109bc1442cf466e49cbaae60ee8f6210
Dani723 <dani@dk2.de>
2016-08-25 11:27:34 +0200

    build fixes

--- dist/index.js
+++ dist/index.js
@@ -1,65 +1,55 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-var _angular_core = require('@angular/core');
-
-function __decorate(decorators, target, key, desc) {
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
-}
-
-exports.IconService = class IconService {
-    fa(icon) {
-        return `fa fa-${icon}`;
-    }
-    lookup(icon) {
-        let className = '';
-        if (typeof icon === 'string' && icon) {
-            let iconName = icon;
-            let providerName;
-            let iconParts = iconName.split(':');
-            if (iconParts.length > 1) {
-                providerName = iconParts[0];
-                iconName = iconParts[1];
-            }
-            else {
-                providerName = 'fa';
-            }
-            if (!this[providerName]) {
-                throw new Error('Invalid icon provider: ' + providerName);
-            }
-            return this[providerName](iconName);
-        }
-        return null;
-    }
 };
-exports.IconService = __decorate([
-    _angular_core.Injectable()
-], exports.IconService);
-
-// @NgModule({
-//   imports: [
-//     VCLIconModule,
-//     VCLIcogramModule,
-//     VCLButtonModule,
-//     VCLLayerModule,
-//     VCLTetherModule,
-//     VCLPopoverModule,
-//   ],
-//   exports: [
-//     VCLIconModule,
-//     VCLIcogramModule,
-//     VCLButtonModule,
-//     VCLLayerModule,
-//     VCLTetherModule,
-//     VCLPopoverModule,
-//   ],
-//   providers: [
-//     // IconService,
-//     OverlayManagerService
-//   ],
-// })
-// export class VCLModule { }
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+var core_1 = require('@angular/core');
+var icon_module_1 = require('./components/icon/icon.module');
+var icogram_module_1 = require('./components/icogram/icogram.module');
+var button_module_1 = require('./components/button/button.module');
+var layer_module_1 = require('./components/layer/layer.module');
+var tether_module_1 = require('./components/tether/tether.module');
+var popover_module_1 = require('./components/popover/popover.module');
+__export(require('./components/icon/icon.module'));
+__export(require('./components/icogram/icogram.module'));
+__export(require('./components/button/button.module'));
+__export(require('./l10n/l10n.module'));
+var overlayManager_service_1 = require('./services/overlayManager.service');
+var VCLModule = (function () {
+    function VCLModule() {
+    }
+    VCLModule = __decorate([
+        core_1.NgModule({
+            imports: [
+                icon_module_1.VCLIconModule,
+                icogram_module_1.VCLIcogramModule,
+                button_module_1.VCLButtonModule,
+                layer_module_1.VCLLayerModule,
+                tether_module_1.VCLTetherModule,
+                popover_module_1.VCLPopoverModule,
+            ],
+            exports: [
+                icon_module_1.VCLIconModule,
+                icogram_module_1.VCLIcogramModule,
+                button_module_1.VCLButtonModule,
+                layer_module_1.VCLLayerModule,
+                tether_module_1.VCLTetherModule,
+                popover_module_1.VCLPopoverModule,
+            ],
+            providers: [
+                overlayManager_service_1.OverlayManagerService
+            ],
+        }), 
+        __metadata('design:paramtypes', [])
+    ], VCLModule);
+    return VCLModule;
+}());
+exports.VCLModule = VCLModule;
