a57fffbd2b40441c06766189f49ce700a782fe3f
Dani723 <dani@dk2.de>
2016-09-02 10:59:47 +0200

    build

--- scripts/copy-html.js
+++ scripts/copy-html.js
@@ -1,15 +1,15 @@
 var cpr = require('cpr');
 
 cpr('src', 'dist', {
-  overwrite: true,    
+  overwrite: true,
   filter: /^(?!.*.html$).*$/
 }, function(err, files) {
     if(err) console.log(err);
 });
 
-cpr('src', 'dist/es', {
-  overwrite: true,    
-  filter: /^(?!.*.html$).*$/
-}, function(err, files) {
-    if(err) console.log(err);
-});
+// cpr('src', 'dist/es', {
+//   overwrite: true,
+//   filter: /^(?!.*.html$).*$/
+// }, function(err, files) {
+//     if(err) console.log(err);
+// });
