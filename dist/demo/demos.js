ce66814cd634689c37e312542ff9f4e3db8e17bc
danielsun174 <daniel_van_meyer@hotmail.de>
2016-11-03 16:35:32 +0100

    ADD dist-files

---
+++ dist/demo/demos.js
@@ -0,0 +1,84 @@
+"use strict";
+var demo_component_1 = require("./components/demo/demo.component");
+var metalist_demo_1 = require("./components/metalist/metalist.demo");
+var dropdown_demo_1 = require("./components/dropdown/dropdown.demo");
+var select_demo_1 = require("./components/select/select.demo");
+var icon_demo_1 = require("./components/icon/icon.demo");
+var icogram_demo_1 = require("./components/icogram/icogram.demo");
+var button_demo_1 = require("./components/button/button.demo");
+var button_group_demo_1 = require("./components/button-group/button-group.demo");
+var layer_demo_1 = require("./components/layer/layer.demo");
+var tether_demo_1 = require("./components/tether/tether.demo");
+var link_demo_1 = require("./components/link/link.demo");
+var radio_button_demo_1 = require("./components/radio-button/radio-button.demo");
+var checkbox_demo_1 = require("./components/checkbox/checkbox.demo");
+var form_control_label_demo_1 = require("./components/form-control-label/form-control-label.demo");
+var input_demo_1 = require("./components/input/input.demo");
+var popover_demo_1 = require("./components/popover/popover.demo");
+var tab_nav_demo_1 = require("./components/tab-nav/tab-nav.demo");
+var navigation_demo_1 = require("./components/navigation/navigation.demo");
+var toolbar_demo_1 = require("./components/toolbar/toolbar.demo");
+var wormhole_demo_1 = require("./components/wormhole/wormhole.demo");
+var off_click_demo_1 = require("./components/off-click/off-click.demo");
+var month_picker_demo_1 = require("./components/month-picker/month-picker.demo");
+var month_selector_demo_1 = require("./components/month-selector/month-selector.demo");
+var l10n_demo_1 = require("./components/l10n/l10n.demo");
+exports.DEMOS = [
+    metalist_demo_1.default,
+    dropdown_demo_1.default,
+    select_demo_1.default,
+    icon_demo_1.default,
+    icogram_demo_1.default,
+    button_demo_1.default,
+    button_group_demo_1.default,
+    layer_demo_1.default,
+    tether_demo_1.default,
+    link_demo_1.default,
+    radio_button_demo_1.default,
+    checkbox_demo_1.default,
+    form_control_label_demo_1.default,
+    input_demo_1.default,
+    popover_demo_1.default,
+    tab_nav_demo_1.default,
+    navigation_demo_1.default,
+    toolbar_demo_1.default,
+    wormhole_demo_1.default,
+    off_click_demo_1.default,
+    month_picker_demo_1.default,
+    month_selector_demo_1.default,
+    l10n_demo_1.default,
+];
+exports.GROUPED_DEMOS = function () {
+    var itemsMap = {};
+    exports.DEMOS.forEach(function (c) {
+        if (itemsMap[c.category]) {
+            itemsMap[c.category].push({
+                label: c.name,
+                route: ['/' + c.path],
+                active: true,
+            });
+        }
+        else {
+            itemsMap[c.category] = [{
+                    label: c.name,
+                    route: ['/' + c.path],
+                    active: true,
+                }];
+        }
+    });
+    return Object.keys(itemsMap).map(function (category) { return ({
+        label: category,
+        items: itemsMap[category],
+        active: true,
+    }); });
+}();
+exports.DEMO_DECLARATIONS = exports.DEMOS.map(function (dc) { return Object.keys(dc.tabs)
+    .map(function (key) { return dc.tabs[key]; })
+    .filter(function (o) { return typeof o === 'function'; }); });
+exports.DEMO_ROUTES = (exports.DEMOS.map(function (dc) {
+    return {
+        path: dc.path,
+        component: demo_component_1.DemoComponent,
+        data: dc
+    };
+}));
---
+++ dist/src/components/navigation/navigation.component.js
@@ -0,0 +1,227 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var router_1 = require("@angular/router");
+var core_1 = require("@angular/core");
+var NavigationItemComponent = (function () {
+    function NavigationItemComponent() {
+        this.active = true;
+        this.selected = false;
+        this.opened = false;
+        this.heading = false;
+    }
+    NavigationItemComponent.prototype.toObject = function () {
+        var ret = {
+            label: this.label,
+            active: this.active,
+            selected: this.selected,
+            opened: this.opened,
+            heading: this.heading,
+            href: this.href,
+            prepIcon: this.prepIcon,
+            appIcon: this.appIcon
+        };
+        if (this.route)
+            ret['route'] = [this.route];
+        var items = [];
+        var ar = this.items.toArray();
+        ar.shift();
+        ar.map(function (navItemCom) { return items.push(navItemCom.toObject()); });
+        if (items.length > 0)
+            ret['items'] = items;
+        return ret;
+    };
+    return NavigationItemComponent;
+}());
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Object)
+], NavigationItemComponent.prototype, "label", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Object)
+], NavigationItemComponent.prototype, "route", void 0);
+__decorate([
+    core_1.ContentChildren(NavigationItemComponent),
+    __metadata("design:type", core_1.QueryList)
+], NavigationItemComponent.prototype, "items", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Boolean)
+], NavigationItemComponent.prototype, "active", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Boolean)
+], NavigationItemComponent.prototype, "selected", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Object)
+], NavigationItemComponent.prototype, "opened", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Object)
+], NavigationItemComponent.prototype, "heading", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Object)
+], NavigationItemComponent.prototype, "href", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Object)
+], NavigationItemComponent.prototype, "prepIcon", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Object)
+], NavigationItemComponent.prototype, "appIcon", void 0);
+NavigationItemComponent = __decorate([
+    core_1.Directive({
+        selector: 'vcl-navitem'
+    }),
+    __metadata("design:paramtypes", [])
+], NavigationItemComponent);
+exports.NavigationItemComponent = NavigationItemComponent;
+var NavigationComponent = (function () {
+    function NavigationComponent(router) {
+        this.router = router;
+        this.ariaRole = 'presentation';
+        this.tabindex = 0;
+        this.touchAction = 'pan-y';
+        this.type = 'horizontal';
+        this.subLevelHintIconClosed = 'fa:chevron-right';
+        this.subLevelHintIconOpened = 'fa:chevron-down';
+        this.subLevelHintIconSide = 'right';
+        this.navigationItems = [];
+        this.select = new core_1.EventEmitter();
+    }
+    NavigationComponent.prototype.ngAfterContentInit = function () {
+        var templateItemsAr = this.templateItems.toArray();
+        if (templateItemsAr.length > 0) {
+            var items_1 = [];
+            templateItemsAr.map(function (i) { return items_1.push(i.toObject()); });
+            this.navigationItems = items_1;
+        }
+        var selectedItem = this._navigationItems.filter(function (item) { return item.selected; })[0];
+        if (selectedItem) {
+            this.selectItem(selectedItem);
+        }
+    };
+    Object.defineProperty(NavigationComponent.prototype, "_navigationItems", {
+        get: function () {
+            return this.navigationItems.filter(function (item) { return item.active; });
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(NavigationComponent.prototype, "isVertical", {
+        get: function () {
+            return this.type === 'vertical';
+        },
+        enumerable: true,
+        configurable: true
+    });
+    NavigationComponent.prototype.getPrepIcon = function (item) {
+        return item.items && this.subLevelHintIconSide === 'left'
+            ? item.opened
+                ? this.subLevelHintIconOpened
+                : this.subLevelHintIconClosed
+            : item.prepIcon;
+    };
+    NavigationComponent.prototype.getAppIcon = function (item) {
+        return item.items && this.subLevelHintIconSide === 'right'
+            ? item.opened
+                ? this.subLevelHintIconOpened
+                : this.subLevelHintIconClosed
+            : item.appIcon;
+    };
+    NavigationComponent.prototype.selectItem = function (item) {
+        if (item == this.selectedItem || item.items) {
+            return;
+        }
+        if (this.selectedItem) {
+            this.selectedItem.selected = false;
+        }
+        item.selected = true;
+        this.selectedItem = item;
+        if (item.href) {
+            window.location.href = item.href;
+        }
+        else if (item.route) {
+            this.router.navigate(item.route);
+        }
+        this.select.emit(item);
+    };
+    NavigationComponent.prototype.onSelect = function (item) {
+        if (this.selectedItem) {
+            this.selectedItem.selected = false;
+        }
+        this.selectedItem = item;
+        this.select.emit(item);
+    };
+    NavigationComponent.prototype.toggleMenu = function (item) {
+        item.opened = !item.opened;
+    };
+    return NavigationComponent;
+}());
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", String)
+], NavigationComponent.prototype, "ident", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Object)
+], NavigationComponent.prototype, "selectedItem", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", String)
+], NavigationComponent.prototype, "ariaRole", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Number)
+], NavigationComponent.prototype, "tabindex", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", String)
+], NavigationComponent.prototype, "touchAction", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", String)
+], NavigationComponent.prototype, "type", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", String)
+], NavigationComponent.prototype, "subLevelHintIconClosed", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", String)
+], NavigationComponent.prototype, "subLevelHintIconOpened", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", String)
+], NavigationComponent.prototype, "subLevelHintIconSide", void 0);
+__decorate([
+    core_1.ContentChildren(NavigationItemComponent),
+    __metadata("design:type", core_1.QueryList)
+], NavigationComponent.prototype, "templateItems", void 0);
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", Array)
+], NavigationComponent.prototype, "navigationItems", void 0);
+__decorate([
+    core_1.Output(),
+    __metadata("design:type", Object)
+], NavigationComponent.prototype, "select", void 0);
+NavigationComponent = __decorate([
+    core_1.Component({
+        selector: 'vcl-navigation',
+        templateUrl: 'navigation.component.html',
+    }),
+    __metadata("design:paramtypes", [router_1.Router])
+], NavigationComponent);
+exports.NavigationComponent = NavigationComponent;
