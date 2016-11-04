f3d4dc99925d793577d8e5ff7722483a60951344
Druvis Cukurs <druvis.cukurs@gmail.com>
2016-11-04 15:24:43 +0200

    minWidth up

--- dist/components/select/select.component.js
+++ dist/components/select/select.component.js
@@ -45,7 +45,7 @@ var SelectComponent = (function () {
         }
         // Adjust min input width to fit it's content.
         this.hide.nativeElement.innerHTML = this.displayValue;
-        this.input.nativeElement.style.minWidth = this.hide.nativeElement.style.width;
+        this.input.nativeElement.style.minWidth = (this.hide.nativeElement.offsetWidth * 1.5) + 'px';
     };
     SelectComponent.prototype.onOutsideClick = function (event) {
         this.expanded = false;
