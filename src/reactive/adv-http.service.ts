405faa0323d04b7fdbccacf5f0da34d3f79d8153
Dani723 <dani@dk2.de>
2016-11-10 16:16:09 +0100

    Minor fix

--- src/reactive/adv-http.service.ts
+++ src/reactive/adv-http.service.ts
@@ -218,7 +218,7 @@ export declare interface AdvHttpConfig {
     }
   ]
 })
-export class AdvHttpModule { 
+export class AdvHttpModule {
   static forRoot(config: AdvHttpConfig): ModuleWithProviders {
     return {
       ngModule: AdvHttpModule,
