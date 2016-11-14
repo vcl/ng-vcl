23fecc0668a1fbc1995d036af2af0ed254012d13
Dani723 <dani@dk2.de>
2016-11-14 12:58:00 +0100

    Destroy tether instance when view is destroyed. Closes #1.

--- src/components/tether/tether.component.ts
+++ src/components/tether/tether.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input, Output, ElementRef } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Component, Input, Output, ElementRef, EventEmitter } from '@angular/core';
 import * as Tether from 'tether';
+
+let tetherID = 10000;
 
 @Component({
   selector: 'vcl-tether',
@@ -24,21 +27,35 @@ export class TetherComponent {
   @Input()
   attachment: string;
 
+  tether: Tether;
+
+  _error = new EventEmitter<any>();
+  @Output()
+  get error(): Observable<any> {
+    return this._error.asObservable();
+  }
+
   constructor(private myElement: ElementRef) {
-    this.id = 'tetherId' + Math.floor(Math.random()*10000);
+    this.id = 'vcl-tetherId' + tetherID++;
   }
 
   ngAfterViewInit() {
     try {
-      new Tether({
+      this.tether = new Tether({
         element: '#' + this.id,
         target: this.target,
         attachment: this.attachment,
         targetAttachment: this.targetAttachment
       });
     } catch (ex) {
-      console.log(ex);
+      this._error.emit(ex);
     }
+  }
+
+  ngOnDestroy() {
+    try {
+      this.tether && this.tether.destroy();
+    } catch (ex) { }
   }
 }
 
