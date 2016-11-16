27ce79b6d7a101b6d9fc1c04e91eb17cd325fae7
danielsun174 <daniel_van_meyer@hotmail.de>
2016-11-16 11:27:10 +0100

    ADD(dropdown) custom item-class

--- demo/components/dropdown/dropdown.component.ts
+++ demo/components/dropdown/dropdown.component.ts
@@ -16,7 +16,7 @@ export class DropdownComponent implements OnInit {
   items: any[] = [
     { label: 'item 1' },
     { label: 'item 2' },
-    { label: 'item 3' },
+    { label: 'item 3', class: 'myclass myclass2' },
     { label: 'item 4' },
     { label: 'item 5' },
     { label: 'item 6', sublabel: 'sublabel of item 6' },
@@ -24,10 +24,10 @@ export class DropdownComponent implements OnInit {
     { label: 'item 8', sublabel: 'sublabel of item 8' },
     { label: 'item 9' },
     { label: 'item 10' }
-  ]
+  ];
 
   onSelect(selectedItems: any[]) {
-    console.log(selectedItems);
+    // console.log(selectedItems);
     if (selectedItems && selectedItems[0]) {
       this.selectedItem = selectedItems[0];
     } else {
