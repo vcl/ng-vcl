11939b1cfc154020f67d83d97922649cd62ac21c
Dani723 <dani@dk2.de>
2017-01-13 14:08:16 +0100

    demo: fuse.js workaround

--- demo/app.component.ts
+++ demo/app.component.ts
@@ -3,9 +3,14 @@ import { Router } from '@angular/router';
 import { Store, StoreErrorAction } from '../src/index';
 import { GROUPED_DEMOS } from "./demos";
 
-// TODO: update typedef for fuse.js
-// https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/fuse
-import { Fuse }  from 'fuse.js';
+// TODO: temp fix for fuse.js import until fix is integrated
+// https://github.com/krisk/Fuse/pull/129
+interface FuseStub {
+  new(...args): FuseStub;
+  search(...args): FuseStub;
+  reduce<T>(...args);
+}
+const Fuse = require('fuse.js') as FuseStub;
 
 @Component({
   selector: 'app',
