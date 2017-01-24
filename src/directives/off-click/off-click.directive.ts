2afa8fdc6205cce38e7394ee7c5e013935b8970e
Dani723 <dani@dk2.de>
2017-01-24 13:49:35 +0100

    off-click: improvements

--- src/directives/off-click/off-click.directive.ts
+++ src/directives/off-click/off-click.directive.ts
@@ -1,3 +1,7 @@
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/observable/fromEvent';
 import { NgModule, EventEmitter, Output, Directive, ElementRef} from '@angular/core';
 
 @Directive({
@@ -7,31 +11,29 @@ export class OffClickDirective {
   @Output('off-click')
   offClick = new EventEmitter();
 
-  private listener: { (ev: MouseEvent) };
+  private sub: Subscription;
 
   constructor(private elem: ElementRef) { }
 
   ngAfterViewInit() {
     if (typeof document !== 'undefined') {
-      // Create the listener
-      this.listener = (ev) => {
+      // Add a small delay, so the current click does not trigger the off-click
+      const delay$ =  Observable.timer(50).first();
+
+      this.sub = Observable.fromEvent<any>(document, 'click')
+                           .skipUntil(delay$)
+                           .subscribe(ev => {
         const me = this.elem.nativeElement;
         // Check if the target is the off-clicks element or an sub element 
         if (ev.target && me !== ev.target && !me.contains(ev.target)) {
           this.offClick.emit();
         }
-      };
-      // Wait for next run loop to attach the listener as it might be triggered by a current click event
-      setTimeout(() => {
-        document.addEventListener('click', this.listener);
-      }, 0);
+      });
     }
   }
 
   ngOnDestroy() {
-    if (typeof document !== 'undefined' && this.listener) {
-      document.removeEventListener('click', this.listener);
-    }
+    this.sub && this.sub.unsubscribe();
   }
 }
 
