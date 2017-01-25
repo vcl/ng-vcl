029fbb0851d728afc99fabdf8f5f20e23a44abf9
Dani723 <dani@dk2.de>
2017-01-25 12:54:35 +0100

    off-click: add some off-click notes regarding touch-chrome

--- src/directives/off-click/off-click.directive.ts
+++ src/directives/off-click/off-click.directive.ts
@@ -17,7 +17,14 @@ export class OffClickDirective {
 
   ngAfterViewInit() {
     if (typeof document !== 'undefined') {
-      // Add a small delay, so the current click does not trigger the off-click
+      // A workaround for touch/or emulated touch chrome
+      // Sometimes chrome has the wrong event target when an element is rendered over or near the
+      // tap's source element within a short time period (about 30ms)
+      // See https://bugs.chromium.org/p/chromium/issues/detail?id=399761
+
+      // Add a small delay, so any click that causes this directive to render, does not trigger an off-click
+      // This does not resolve the problem when a later tap inside the element should not trigger the offClick,
+      // but the events target is wrong
       const delay$ =  Observable.timer(50).first();
 
       this.sub = Observable.fromEvent<any>(document, 'click')
