f44212d0e97ebfe7684f87961ac44c7d9054639e
Dani723 <dani@dk2.de>
2017-01-26 13:02:19 +0100

    off-click: replace document click event with hammerjs singletap

--- src/directives/off-click/off-click.directive.ts
+++ src/directives/off-click/off-click.directive.ts
@@ -1,8 +1,9 @@
+import { NgModule, EventEmitter, Output, Directive, ElementRef } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/timer';
-import 'rxjs/add/observable/fromEvent';
-import { NgModule, EventEmitter, Output, Directive, ElementRef} from '@angular/core';
+import 'rxjs/add/observable/fromEventPattern';
+import * as Hammer from 'hammerjs';
 
 @Directive({
   selector: '[off-click]',
@@ -17,19 +18,13 @@ export class OffClickDirective {
 
   ngAfterViewInit() {
     if (typeof document !== 'undefined') {
-      // A workaround for touch/or emulated touch chrome
-      // Sometimes chrome has the wrong event target when an element is rendered over or near the
-      // tap's source element within a short time period (about 30ms)
-      // See https://bugs.chromium.org/p/chromium/issues/detail?id=399761
+      let mc = new Hammer.Manager(document);
+      mc.add( new Hammer.Tap({ event: 'singletap' }) );
 
-      // Add a small delay, so any click that causes this directive to render, does not trigger an off-click
-      // This does not resolve the problem when a later tap inside the element should not trigger the offClick,
-      // but the events target is wrong
-      const delay$ =  Observable.timer(50).first();
-
-      this.sub = Observable.fromEvent<any>(document, 'click')
-                           .skipUntil(delay$)
-                           .subscribe(ev => {
+      this.sub = Observable.fromEventPattern<any>(
+        handler => mc.on('singletap', handler),
+        handler => mc.off('singletap', handler)
+      ).subscribe(ev => {
         const me = this.elem.nativeElement;
         // Check if the target is the off-clicks element or an sub element 
         if (ev.target && me !== ev.target && !me.contains(ev.target)) {
