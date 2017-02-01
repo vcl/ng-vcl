03915d7dc3ab67a814e15704d87c0c3dbbd2480b
pubkey <daniel_van_meyer@hotmail.de>
2017-02-01 15:57:41 +0100

    FIX(input-control-group) todo

--- src/components/input-control-group/input-control-group.component.ts
+++ src/components/input-control-group/input-control-group.component.ts
@@ -1,12 +1,14 @@
 import {
   OnInit, Component, Input,
   ViewChildren,
-  EventEmitter, Output, HostListener, ElementRef
+  EventEmitter, Output, HostListener, ElementRef,
+  ChangeDetectionStrategy
 } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'vcl-input-control-group',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     '[class.vclInputControlGroup]': 'true',
   },
@@ -18,52 +20,14 @@ export class InputControlGroup implements OnInit {
   @Input('type') type: 'error' | 'warning' | 'success' = null;
   @Input('label') label: string;
 
-
-  /*  @Input('message')
-    message: Observable<{
-      type: 'error' | 'warning' | 'success', value: string
-    }>;
-    type$; value$; classname$; classes$;
-    sub;
-  */
-
   constructor(private elRef: ElementRef) {
     this.elRef = elRef;
   }
 
-  ngOnInit() {
+  ngOnInit() { }
+  ngOnChanges(changes: any) { }
 
-
-    // TODO(dmeyer) this is commented out because i will switch from passing streams to the default 2-way-binding
-    /*
-        if (!this.message) return;
-
-        this.type$ = this.message.map(msg => msg.type || null);
-        this.value$ = this.message.map(msg => msg.value || null);
-        this.classname$ = this.type$.map(t => {
-          if (!t) return '';
-          return 'vcl' + this.ucfirst(t);
-        });
-        this.classes$ = this.classname$.map(name => 'vclFormControlHint ' + name);
-
-        this.sub = this.classname$.subscribe(newClassname => {
-          let els: any[] = Array.from(this.elRef.nativeElement.children)
-            .slice(0, -1); // remove last el because it is the message-box
-          els = els.filter(el => el.classList.contains('vclInput')); // TODO add more
-          els.forEach(el => {
-            el.classList.remove('vclError');
-            el.classList.remove('vclWarning');
-            el.classList.remove('vclSuccess');
-            if (newClassname != '') el.classList.add(newClassname);
-          });
-        });
-    */
-
-  }
-
-  ngOnDestroy() {
-    // if (this.sub) this.sub.unsubscribe();
-  }
+  ngOnDestroy() { }
 
   ucfirst(str: string): string {
     return str.charAt(0).toUpperCase() + str.slice(1);
