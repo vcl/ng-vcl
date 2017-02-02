70bf582d56ccf90f0728b3f6501f36f731262ebf
Dani723 <dani@dk2.de>
2017-02-02 09:36:40 +0100

    vcl-input: fixes

--- src/components/input/input.directive.ts
+++ src/components/input/input.directive.ts
@@ -1,8 +1,5 @@
-import { OnInit, OnDestroy, Directive, Input,
-  EventEmitter, Output, HostListener,
-  ElementRef, ViewChild, forwardRef } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Directive, Input, Output, ElementRef, HostListener, EventEmitter } from '@angular/core';
 
 // Invalid input type. Using one of these will throw an error
 const INPUT_INVALID_TYPES = [
@@ -24,49 +21,56 @@ const INPUT_INVALID_TYPES = [
     '[class.vclInput]': 'true',
   }
 })
-export class InputDirective implements OnInit, OnDestroy {
+export class InputDirective {
 
-  @Input('state') state: 'error' | 'warning' | 'success';
-  @Input('type') type: string = 'text';
-  @Input() selectOnFocus: boolean = false;
+  @Input('state')
+  state: 'error' | 'warning' | 'success';
 
-  @Input('value') value;
-  _valueSubject = new BehaviorSubject<any>(this.value);
-  @Output('typedValueChange') typedValueChange = this._valueSubject.asObservable();
+  @Input('type')
+  type: string = 'text';
 
-  subs = [];
+  @Input()
+  selectOnFocus: boolean = false;
 
-  constructor(private elRef: ElementRef) {
-    if (INPUT_INVALID_TYPES.includes(this.type))
-      throw new Error('type not allowed for vcl-input: ' + this.type);
+  @Input()
+  set value(value: any) {
+    this.typedValue = this.toType(value);
+  }
+  get value() {
+    return this.typedValue;
   }
 
+  private typedValue: any;
+
+  private valueChangeEvent = new EventEmitter<any>();
+  @Output()
+  valueChange = this.valueChangeEvent.asObservable();
+
+  constructor(private elRef: ElementRef) { }
+
   ngOnInit() {
+    if (INPUT_INVALID_TYPES.includes(this.type))
+      throw new Error('type not allowed for vcl-input: ' + this.type);
   }
 
   toType(value) {
     if (this.type === 'number') {
       let tValue = Number(value);
       return isNaN(tValue) ? 0 : tValue;
-    } else return value;
+    } else {
+      return value;
+    }
   }
 
   @HostListener('input', ['$event.target.value'])
   onChange(value) {
-    this._valueSubject.next(this.toType(value));
+    this.valueChangeEvent.next(this.toType(value));
   }
 
   @HostListener('focus', ['$event.target.value'])
   onFocus(value) {
-    if (this.selectOnFocus) {
-      if (this.elRef && this.elRef.nativeElement) {
-        this.elRef.nativeElement.select();
-      }
+    if (this.selectOnFocus && this.elRef && this.elRef.nativeElement) {
+      this.elRef.nativeElement.select();
     }
   }
-
-  ngOnDestroy() {
-    this.subs.forEach(sub => sub.unsubscribe());
-  }
-
 }
