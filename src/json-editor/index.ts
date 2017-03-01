4052bb894f3f012389cfbe73a465415d439ffb5e
Dani723 <dani@dk2.de>
2017-03-01 14:08:38 +0100

    json-editor: remove ng-vcl dep

--- src/json-editor/index.ts
+++ src/json-editor/index.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { VCLMetalistModule, L10nModule } from '@ng-vcl/ng-vcl';
 import { JsonEditorComponent } from './json-editor.component';
 
 export * from './json-editor.component';
 
 @NgModule({
-  imports: [CommonModule, L10nModule, VCLMetalistModule],
+  imports: [CommonModule],
   exports: [JsonEditorComponent],
   declarations: [JsonEditorComponent],
   providers: [],
