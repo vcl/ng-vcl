7dc7952955536c25fb7442f98af41f06fa4f2d04
Druvis <druvis.cukurs@n-fuse.de>
2017-03-07 14:48:26 +0200

    [package.json]: update; [plotly]: update

--- src/plotly/plotly.component.ts
+++ src/plotly/plotly.component.ts
@@ -1,71 +1,70 @@
 import { Component, Input, SimpleChange } from '@angular/core';
-import Plotly from 'plotly.js';
+import * as Plotly from 'plotly.js';
 
 @Component({
   selector: 'vcl-plotly',
   templateUrl: './plotly.component.html'
 })
 export class PlotlyComponent {
-  private TAG: string = 'PlotlyComponent';
+  private static readonly Tag = 'PlotlyComponent';
+  private static readonly PlotlyFields: string[] = ['data', 'layout', 'configuration', 'events'];
+  private static readonly RecreateFields: string[] = ['elementId', 'plotClass', 'configuration', 'events'];
 
-  private static readonly plotlyFields: string[] = ['data', 'layout', 'configuration', 'events'];
-  private static readonly recreateFields: string[] = ['elementId', 'plotClass', 'configuration', 'events'];
-
-  private initialized: boolean = false;
+  private tag: string;
+  private initialized = false;
 
   private plot: any;
-
-  @Input() private elementId: string = 'elementId';
-  @Input() private plotClass: string = 'plotlyPlot';
+  @Input() private debug = false;
+  @Input() private elementId: string;
+  @Input() private plotClass: string;
   @Input() private data: any[];
   @Input() private layout: any;
   @Input() private configuration: any;
   @Input() private events: any;
 
-  @Input() private debug: boolean = false;
-
-  ngOnInit() {
-    this.TAG = `${this.TAG}.${this.elementId}`;
+  private ngOnInit(): void {
+    this.tag = `${PlotlyComponent.Tag}.${this.elementId}`;
   }
 
-  ngAfterViewInit() {
-    if (this.debug) console.log(this.TAG, `ngAfterViewInit() initializting`);
-    // Wait one run-loop
-    setTimeout(() => {
-      Plotly.newPlot(this.elementId, this.data, this.layout, this.configuration);
-      this.plot = document.getElementById(this.elementId);
-      this.attachEventListeners(this.elementId, this.plot, this.events);
-      this.initialized = true;
-    }, 0);
+  private ngAfterViewInit(): void {
+    const tag = `${this.tag}.ngAfterViewInit()`;
+    if (this.debug) console.log(tag, `initializting`);
+    Plotly.newPlot(this.elementId, this.data, this.layout, this.configuration);
+    this.plot = document.getElementById(this.elementId);
+    this.attachEventListeners(this.events);
+    this.initialized = true;
+    if (this.debug) console.log(tag, `initialized, this:`, this);
   }
 
-  private attachEventListeners(elementId: string, plot: any, events: any) {
-    Object.keys(events || {}).forEach(eventName => {
-      plot.on(eventName, (event, data) => {
-        events[eventName](data, event, elementId, plot, Plotly);
+  private attachEventListeners(events: any): void {
+    const tag = `${this.tag}.attachEventListeners()`;
+    if (this.debug) console.log(tag, `events:`, events);
+    Object.keys(events || {}).forEach(k => {
+      this.plot.on(k, (data, event) => {
+        if (this.debug) console.log(tag, `called event '${k}'`);
+        events[k](data, event, this.elementId, this.plot, Plotly);
       });
     });
+    if (this.debug) console.log(tag, `this:`, this);
   }
 
-  public addTraces(traces: any | any[], index: number = -1) {
-    if (this.debug) console.log(this.TAG, `addTraces traces (`, traces, `) index (${index !== -1 ? index : this.data.length})`);
-    if (index === -1) {
-      Plotly.addTraces(this.plot, traces);
-    } else {
-      Plotly.addTraces(this.plot, traces, index);
-    }
+  public addTraces(traces: any | any[], index: number = this.data.length): void {
+    const tag = `${this.tag}.addTraces()`;
+    if (this.debug) console.log(tag, `traces (`, traces, `) index (${index})`);
+    Plotly.addTraces(this.plot, traces, index);
   }
 
-  public deleteTraces(traces: number | number[]) {
-    if (this.debug) console.log(this.TAG, `deleteTraces() traces:`, traces);
+  public deleteTraces(traces: number | number[]): void {
+    const tag = `${this.tag}.deleteTraces()`;
+    if (this.debug) console.log(tag, `traces:`, traces);
     Plotly.deleteTraces(this.plot, traces);
   }
 
-  ngOnChanges(changes: any) {
-    if (this.debug) console.log(this.TAG, 'ngOnChanges() changes:', changes);
-
+  private ngOnChanges(changes: any): void {
+    const tag = `${this.tag}.ngOnChanges()`;
+    if (this.debug) console.log(tag, `changes:`, changes);
     if (!this.initialized || !this.plot) {
-      if (this.debug) console.log(this.TAG, `ngOnChanges() ignored changes (initialized - ${this.initialized}, plot - ${this.plot})`);
+      if (this.debug) console.log(tag, `ignored changes (initialized - ${this.initialized}, plot - ${this.plot})`);
       return;
     }
 
@@ -74,10 +73,10 @@ export class PlotlyComponent {
     changedKeys.forEach(k => {
       const change: SimpleChange = changes[k];
       if (change.previousValue !== change.currentValue) {
-        if (this.debug) console.log(this.TAG, `ngOnChanges() ${k} changed from`, change.previousValue, 'to', change.currentValue);
+        if (this.debug) console.log(tag, `${k} changed from`, change.previousValue, 'to', change.currentValue);
         this[k] = change.currentValue;
 
-        const plotlyField: boolean = includes(PlotlyComponent.plotlyFields, k);
+        const plotlyField: boolean = includes(PlotlyComponent.PlotlyFields, k);
         if (plotlyField) {
           this.plot[k] = this[k];
         }
@@ -91,17 +90,17 @@ export class PlotlyComponent {
       }
     });
 
-    if (includesArr(changedKeys, PlotlyComponent.recreateFields)) {
-      // Recreate the plot on recreateFields.
-      if (this.debug) console.log(this.TAG, `ngOnChanges() re-creating, this:`, this);
+    if (includesArr(changedKeys, PlotlyComponent.RecreateFields)) {
+      // Recreate the plot on RecreateFields.
+      if (this.debug) console.log(tag, `re-creating, this:`, this);
       this.ngAfterViewInit();
     } else if (changedKeys.length === 1 && includes(changedKeys, 'layout')) {
       // If only the layout was changed, relayout.
-      if (this.debug) console.log(this.TAG, `ngOnChanges() re-layouting, this:`, this);
+      if (this.debug) console.log(tag, `re-layouting, this:`, this);
       Plotly.relayout(this.plot, this.layout);
     } else {
       // Redraw the plot (data or shapes changed).
-      if (this.debug) console.log(this.TAG, `ngOnChanges() re-drawing, this:`, this);
+      if (this.debug) console.log(tag, `re-drawing, this:`, this);
       Plotly.redraw(this.plot);
     }
   }
