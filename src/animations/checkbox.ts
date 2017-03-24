574d800f6fb509f7d6836af9c181177c9a39f649
Dani723 <dani@dk2.de>
2017-03-24 10:29:34 +0100

    deactivate checkbox/token animations

--- src/animations/checkbox.ts
+++ src/animations/checkbox.ts
@@ -2,21 +2,21 @@ import { trigger, state, transition, style, animate, keyframes } from '@angular/
 import { setAnimations, CheckboxComponent } from '@ng-vcl/ng-vcl';
 
 export function vclCheckboxAnimations() {
-  setAnimations(CheckboxComponent, [
-    trigger('checkState', [
-      state('true', style({ opacity: 1 })),
-      state('false', style({ opacity: 1 })),
-      transition('0 => 1', [
-        animate(600, keyframes([
-          style({ transform: 'scale(1, 1)', offset: 0 }),
-          style({ transform: 'scale(1.25, 0.75)', offset: 0.3 }),
-          style({ transform: 'scale(0.75, 1.25)', offset: 0.4 }),
-          style({ transform: 'scale(1.15, 0.85)', offset: 0.5 }),
-          style({ transform: 'scale(.95, 1.05)', offset: 0.65 }),
-          style({ transform: 'scale(1.05, .95)', offset: 0.75 }),
-          style({ transform: 'scale(1, 1)', offset: 1 })
-        ]))
-      ])
-    ])
-  ]);
+  // setAnimations(CheckboxComponent, [
+  //   trigger('checkState', [
+  //     state('true', style({ opacity: 1 })),
+  //     state('false', style({ opacity: 1 })),
+  //     transition('0 => 1', [
+  //       animate(600, keyframes([
+  //         style({ transform: 'scale(1, 1)', offset: 0 }),
+  //         style({ transform: 'scale(1.25, 0.75)', offset: 0.3 }),
+  //         style({ transform: 'scale(0.75, 1.25)', offset: 0.4 }),
+  //         style({ transform: 'scale(1.15, 0.85)', offset: 0.5 }),
+  //         style({ transform: 'scale(.95, 1.05)', offset: 0.65 }),
+  //         style({ transform: 'scale(1.05, .95)', offset: 0.75 }),
+  //         style({ transform: 'scale(1, 1)', offset: 1 })
+  //       ]))
+  //     ])
+  //   ])
+  // ]);
 };
