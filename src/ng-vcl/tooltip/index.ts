4965496265903fc367c0ae26598ec09602b31827
janis <janis@JANKA-DESKTOPS>
2017-06-05 00:43:23 +0300

    reset appearance on content or position change

--- src/ng-vcl/tooltip/index.ts
+++ src/ng-vcl/tooltip/index.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { TooltipComponent } from './tooltip.component';
 import { TooltipDirective } from './tooltip.directive';
 import { L10nModule } from '../l10n/index';
+import { TooltipService } from './tooltip.service';
 
 @NgModule({
   imports: [CommonModule, L10nModule],
   exports: [TooltipComponent, TooltipDirective],
   declarations: [TooltipComponent, TooltipDirective],
-  providers: [],
+  providers: [TooltipService],
   entryComponents: [TooltipComponent]
 })
 export class VCLTooltipModule { }
