f511704d2a201752a49e2b41cdc5f39a7c39e1fd
Dani723 <dani@dk2.de>
2018-02-19 17:41:40 +0100

    form-control-label: better content detection

--- src/ng-vcl/form-control-label/form-control-label.component.ts
+++ src/ng-vcl/form-control-label/form-control-label.component.ts
@@ -26,21 +26,44 @@ export class FormControlLabelComponent {
   @Input()
   subLabel: string | undefined;
 
-  // Whether the label wraps the control
-  @HostBinding('class.vclFormControlLabelWrapping')
+  // Whether the label wraps a control
   @Input()
-  wrapping = false;
-  // TODO enable when it is possible to determine if there is ng-content
-  // without having to wrap ng-content in another span element
-  // get hasContent() {
-  //   return this.content.nativeElement.childNodes.length > 0;
-  // }
+  wrapping?: boolean;
 
-  // Whether an indicator that an input in to the labelled control is required
+  @HostBinding('class.vclFormControlLabelWrapping')
+  get vclFormControlLabelWrapping() {
+    if (typeof this.wrapping === 'boolean') {
+      return this.wrapping;
+    } else {
+      if (!this.label && !this.subLabel) {
+        // TODO reactivate when it is possible to determine ng-content with no sub-element
+        // If no label is provided wrap when content is defined
+        // This triggers only when content has at least one child element or is text
+        // return this.content && this.content.nativeElement && !isEmpty(this.content.nativeElement);
+        return true;
+      }
+      return false;
+    }
+  }
+
+  // Whether an indicator that an input in to the labeled control is required
   @Input()
   required: boolean = false;
 
   // Accessible label for the required indicator
   @Input()
   requiredIndLabel: string;
+}
+
+function isEmpty(element: HTMLElement): boolean {
+  const nodes = Array.from(element.childNodes);
+  return nodes.every(node => {
+    if (node.nodeType === 3 && node.textContent && node.textContent.trim().length === 0) {
+      return true;
+    }
+    if (node.nodeType === 8) {
+      return true;
+    }
+    return false;
+  });
 }
