b44ab53fe415b109e9412575336bb392b6dbf2f3
alex krez <alexander.krez@n-fuse.de>
2018-02-20 14:41:49 +0700

    Embedded input group demo

---
+++ demo/src/demos/embedded-input-group/demo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'demo.component.html'
+})
+export class EmbeddedInputGroupDemoComponent {
+
+    someAction(event: any): void { console.log(event); }
+
+}
