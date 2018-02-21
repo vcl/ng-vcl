23f6e9e4a995ac9ba2b7196a962aba9192a3a692
Dani723 <dani@dk2.de>
2018-02-21 12:35:08 +0100

    vcl-icogram: support span

--- src/ng-vcl/icogram/icogram.component.ts
+++ src/ng-vcl/icogram/icogram.component.ts
@@ -1,21 +1,39 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy, HostBinding, ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'vcl-icogram, [vcl-icogram]',
+  selector: 'vcl-icogram, div[vcl-icogram], span[vcl-icogram]',
   host: {
     '[class.vclIcogram]': 'true',
-    '[attr.role]': 'img'
+    '[attr.role]': 'img',
   },
   templateUrl: 'icogram.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IcogramComponent {
   @Input() label: string;
+
+  @HostBinding('class.vclHorizontal')
   @Input() flexLabel: boolean;
+
+  @Input() flexLabelCentered: boolean;
+
   @Input() prepIcon: string;
+
   @Input() appIcon: string;
+
   @Input() prepIconSrc: string;
+
   @Input() appIconSrc: string;
 
   constructor(private elRef: ElementRef) { }
+
+  @HostBinding('style.display')
+  get block() {
+    return (!this.span && !this.flexLabel) ? 'block' : null;
+  }
+
+  get span() {
+    return this.elRef.nativeElement && this.elRef.nativeElement.tagName === 'SPAN';
+  }
+
 }
