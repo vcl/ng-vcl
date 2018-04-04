3ae4eab823dac94d086f924c8e8470ee3112ee48
Dani723 <dani@dk2.de>
2018-04-04 13:26:25 +0200

    Fix rollup rxjs globals

--- scripts/rollup-externals.ts
+++ scripts/rollup-externals.ts
@@ -16,10 +16,23 @@ export default {
   '@ng-vcl/plotly': 'ngVCL.plotly',
   '@ng-vcl/animations': 'ngVCL.animations',
 
+  // rxjs dependencies
+  'rxjs/Observable': 'Rx',
+  'rxjs/Subject': 'Rx',
+  'rxjs/BehaviorSubject': 'Rx',
+  'rxjs/operators': 'Rx.operators',
+  'rxjs/observable/of': 'Rx.Observable',
+  'rxjs/observable/from': 'Rx.Observable',
+  'rxjs/observable/never': 'Rx.Observable',
+  'rxjs/observable/combineLatest': 'Rx.Observable',
+  'rxjs/observable/merge': 'Rx.Observable',
+  'rxjs/observable/interval': 'Rx.Observable',
+  'rxjs/observable/timer': 'Rx.Observable',
+  'rxjs/observable/fromEvent': 'Rx.Observable',
+
   // Additional dependencies
   hammerjs: 'Hammer',
   'jsoneditor/dist/jsoneditor.js': 'JSONEditor',
   jsonschema: 'jsonschema',
   'plotly.js': 'Plotly',
 };
-
