4fbd1970426dc623aec1b6bc07019ab1275f5fc1
Daniel Hartmann <contact@dk2.de>
2019-03-04 14:45:57 +0100

    badge: remove .html and module

--- lib/ng-vcl/src/badge/index.ts
+++ lib/ng-vcl/src/badge/index.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BadgeDirective } from './badge.component';
-import { VCLMetalistModule } from '../metalist/index';
 
 @NgModule({
-  imports: [CommonModule, VCLMetalistModule],
+  imports: [CommonModule],
   exports: [BadgeDirective],
   declarations: [BadgeDirective],
   providers: [],
