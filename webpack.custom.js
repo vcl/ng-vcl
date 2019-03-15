ef353bb6ac4271d2f99045ebffe5dadd2df0133e
Daniel Hartmann <contact@dk2.de>
2019-03-15 10:27:29 +0100

    Update to latest vcl and vcl build-tools

--- webpack.custom.js
+++ webpack.custom.js
@@ -1,20 +1,25 @@
 const webpack = require('webpack');
 const path = require('path');
-const fs = require('fs');
-const vcl = require('@vcl/preprocessor');
 const merge = require('webpack-merge');
 const branch = require('git-branch');
+const webpackHelper = require('@ng-vcl/webpack-helper');
 
 module.exports.default = {
-  extractCss: false,
-  sourceMap: false,
+  opts: null,
 
   pre(builderConfig) {
-    this.extractCss = builderConfig.options.extractCss;
-    this.sourceMap = builderConfig.options.sourceMap;
+    this.opts = {
+      sourceMap: builderConfig.options.sourceMap, // Use settings from builder
+      extractCSS: builderConfig.options.extractCss, // Use settings from builder
+    };
   },
 
   config(cfg) {
+    cfg = merge(cfg, webpackHelper({
+      ...this.opts,
+      globalStyle: 'demo/styles.sss',
+      fa: true,
+    }));
 
     const currentBranch = process.env.GIT_BRANCH || branch.sync() || 'unknown';
     
@@ -26,43 +31,7 @@ module.exports.default = {
       });
     }
 
-    // Relative font path in the css
-    const faRelativePath = '../webfonts';
-    // Actual font path
-    const faFontPath = path.join(process.cwd(), './node_modules/@fortawesome/fontawesome-free/webfonts');
-
-    // Read list of fonts
-    const fonts = fs.readdirSync(faFontPath).filter(file => /^\.(eot|ttf|woff|woff2|svg)$/.test(path.extname(file)));
-
-    // Create an alias map for fonts
-    const faAlias = fonts.reduce((alias, filename) => ({
-        ...alias,
-        [faRelativePath + '/' + filename]: path.resolve(process.cwd(), faFontPath, filename)
-    }), {});
-
     return merge(cfg, {
-      resolve: {
-        alias: {
-          ...faAlias,
-        }
-      },
-      module: {
-        rules: [
-          // Rule for global styles
-          vcl.createWebpackRule({
-            include: path.resolve(__dirname, 'demo/styles.sss'),
-            sourceMap: this.sourceMap,
-            extractCSS: this.extractCss
-          }),
-          // Rule for component styles
-          vcl.createWebpackRule({
-            exclude: path.resolve(__dirname, 'demo/styles.sss'),
-            sourceMap: this.sourceMap,
-            extractCSS: false,
-            loader: 'raw-loader'
-          })
-        ]
-      },
       plugins: [
         new webpack.DefinePlugin({
           gitBranch: JSON.stringify(currentBranch)
@@ -71,6 +40,4 @@ module.exports.default = {
     });
   }
 };
-
-
 
