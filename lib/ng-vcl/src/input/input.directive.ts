6e399aa373c060183d205557b8ce3df1425723cf
Daniel Hartmann <contact@dk2.de>
2019-04-05 11:16:42 +0200

    input: upgrade

--- lib/ng-vcl/src/input/input.directive.ts
+++ lib/ng-vcl/src/input/input.directive.ts
@@ -1,13 +1,38 @@
-import { Directive, ElementRef, HostBinding, Input, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, Input, HostListener, forwardRef } from '@angular/core';
+import { FORM_CONTROL_LABEL_MEMBER_TOKEN, FormControlLabelMember } from '../form-control-label';
+
+export let UNIQUE_ID = 0;
+
+export interface VCLInput {
+  readonly isDisabled: boolean;
+  setDisabled(disabled: boolean): void;
+}
 
 @Directive({
   selector: 'input[vclInput]',
-  exportAs: 'vclInput'
+  exportAs: 'vclInput',
+  providers: [
+    {
+      provide: FORM_CONTROL_LABEL_MEMBER_TOKEN,
+      useExisting: forwardRef(() => InputDirective)
+    },
+  ],
 })
-export class InputDirective {
+export class InputDirective implements FormControlLabelMember, VCLInput {
 
   constructor(private elRef: ElementRef<HTMLInputElement>) { }
 
+  private generatedId = 'vcl_input_' + UNIQUE_ID++;
+
+  private _disabled = false;
+
+  @Input()
+  id?: string;
+
+  @HostBinding('attr.id')
+  get elementId() {
+    return this.id || this.generatedId;
+  }
   @Input()
   disabled = false;
 
@@ -19,7 +44,7 @@ export class InputDirective {
 
   @HostBinding('class.vclDisabled')
   get isDisabled() {
-    return this.disabled;
+    return this.disabled || this._disabled;
   }
 
   @HostBinding('attr.disabled')
@@ -33,5 +58,13 @@ export class InputDirective {
     if (this.autoselect) {
       this.elRef.nativeElement.select();
     }
+  }
+
+  setDisabled(disabled: boolean) {
+    this._disabled = disabled;
+  }
+
+  notifyFormControlLabelClick(event: Event): void {
+    this.elRef.nativeElement.focus();
   }
 }
