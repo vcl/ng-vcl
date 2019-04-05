3715a0d945106b2d2983cb97df242118f10b7992
Daniel Hartmann <contact@dk2.de>
2019-04-05 11:53:55 +0200

    Upgrade wc

--- lib/ng-vcl/index.wc.ts
+++ lib/ng-vcl/index.wc.ts
@@ -3,11 +3,11 @@ import {NgModule, Injector} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {createCustomElement} from '@angular/elements';
-import {VCLFlipSwitchModule, FlipSwitchComponent, BusyIndicatorComponent, VCLBusyIndicatorModule} from './index';
+import {VCLFlipSwitchModule, FlipSwitchComponent, BusyIndicatorComponent, VCLBusyIndicatorModule, ButtonComponent, VCLButtonModule} from './index';
 
 @NgModule({
-  imports: [BrowserModule, VCLFlipSwitchModule, VCLBusyIndicatorModule],
-  entryComponents: [FlipSwitchComponent, BusyIndicatorComponent]
+  imports: [BrowserModule, VCLFlipSwitchModule, VCLBusyIndicatorModule, VCLButtonModule],
+  entryComponents: [FlipSwitchComponent, BusyIndicatorComponent, ButtonComponent]
 })
 export class WSModule {
   constructor(private injector: Injector) { }
@@ -17,6 +17,10 @@ export class WSModule {
     customElements.define('vcl-flip-switch', elm);
     elm = createCustomElement(BusyIndicatorComponent, {injector: this.injector});
     customElements.define('vcl-busy-indicator', elm);
+    elm = createCustomElement(ButtonComponent, {injector: this.injector});
+    customElements.define('vcl-button', elm, {
+      extends: 'button'
+    });
   }
 }
 
