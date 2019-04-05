0f3876714e5447a5244d9d1c17419f49bdd5b8f7
HenriARM <henriarm@yandex.ru>
2019-04-05 16:20:42 +0300

    Fa problem

--- lib/ng-vcl/index.wc.ts
+++ lib/ng-vcl/index.wc.ts
@@ -3,11 +3,19 @@ import {NgModule, Injector} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {createCustomElement} from '@angular/elements';
-import {VCLFlipSwitchModule, FlipSwitchComponent, BusyIndicatorComponent, VCLBusyIndicatorModule, ButtonComponent, VCLButtonModule} from './index';
+import {
+  VCLFlipSwitchModule,
+  FlipSwitchComponent,
+  BusyIndicatorComponent,
+  VCLBusyIndicatorModule,
+  ButtonComponent,
+  VCLButtonModule,
+  ButtonIconComponent
+} from './index';
 
 @NgModule({
   imports: [BrowserModule, VCLFlipSwitchModule, VCLBusyIndicatorModule, VCLButtonModule],
-  entryComponents: [FlipSwitchComponent, BusyIndicatorComponent, ButtonComponent]
+  entryComponents: [FlipSwitchComponent, BusyIndicatorComponent, ButtonComponent, ButtonIconComponent]
 })
 export class WSModule {
   constructor(private injector: Injector) { }
@@ -19,6 +27,11 @@ export class WSModule {
     customElements.define('vcl-busy-indicator', elm);
     elm = createCustomElement(ButtonComponent, {injector: this.injector});
     customElements.define('vcl-button', elm, {
+      extends: 'button'
+    });
+
+    elm = createCustomElement(ButtonIconComponent, {injector: this.injector});
+    customElements.define('vcl-icon-button', elm, {
       extends: 'button'
     });
   }
