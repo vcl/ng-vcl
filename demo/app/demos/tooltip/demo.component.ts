9c991c0d4a3b47b8d476ecb352d145ad5cbce3d7
Daniel Hartmann <contact@dk2.de>
2019-05-08 14:45:04 +0200

    Remove tooltip demo

--- demo/app/demos/tooltip/demo.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    templateUrl: './demo.component.html',
-    styles: [`p{ text-align:center;padding-top:30px}`]
-})
-export class ToolTipDemoComponent  {
-
-}
