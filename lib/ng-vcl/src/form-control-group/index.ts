24152a5a5020c5ec79cd0c7440fbb27589aa31a8
Daniel Hartmann <contact@dk2.de>
2019-05-24 16:34:46 +0200

    form-control-group: add error state agent

--- lib/ng-vcl/src/form-control-group/index.ts
+++ lib/ng-vcl/src/form-control-group/index.ts
@@ -7,15 +7,22 @@ import { FormControlLabelDirective } from './form-control-label.directive';
 import { FormControlRequiredComponent } from './required.component';
 
 export { FormControlGroupComponent, FORM_CONTROL_INPUT, FormControlInput, FormControlErrorStateAgent, FORM_CONTROL_ERROR_STATE_AGENT, FormControlHost, FORM_CONTROL_HOST };
-
 // tslint:disable-next-line:only-arrow-functions
 export function defaultFormControlErrorStateAgent(form?, input?) {
   return form &&
          input &&
-         input .ngControl &&
-         !input.isFocused &&
+         input.ngControl &&
          input.ngControl.invalid &&
          (input.ngControl.touched || form.submitted);
+}
+
+// tslint:disable-next-line:only-arrow-functions
+export function dirtyFormControlErrorStateAgent(form?, input?) {
+  return form &&
+         input &&
+         input .ngControl &&
+         input.ngControl.invalid &&
+         (input.ngControl.dirty || form.submitted);
 }
 
 @NgModule({
