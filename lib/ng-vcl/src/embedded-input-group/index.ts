c5750f8753966a7349d234aec72124e1948e87bd
Daniel Hartmann <contact@dk2.de>
2020-01-14 12:59:06 +0100

    fix(embedded-input-group): add missing imports

--- lib/ng-vcl/src/embedded-input-group/index.ts
+++ lib/ng-vcl/src/embedded-input-group/index.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EmbeddedInputGroupComponent } from './embedded-input-group.component';
 import { VCLCoreContentProjectionModule } from '../core/index';
+import { VCLIconModule } from '../icon/index';
+import { VCLInputModule } from '../input/index';
+import { EmbeddedInputGroupComponent } from './embedded-input-group.component';
 
 export { EmbeddedInputGroupComponent };
 @NgModule({
-  imports: [CommonModule, VCLCoreContentProjectionModule],
-  exports: [EmbeddedInputGroupComponent, VCLCoreContentProjectionModule ],
+  imports: [CommonModule, VCLCoreContentProjectionModule, VCLInputModule, VCLIconModule],
+  exports: [EmbeddedInputGroupComponent, VCLCoreContentProjectionModule, VCLInputModule, VCLIconModule ],
   declarations: [EmbeddedInputGroupComponent],
 })
 export class VCLEmbeddedInputGroupModule { }
