2bc28a27c8aa4a902ed9750e2bae9834cc58ba2a
Bello Babakolo <fleetbeekay@gmail.com>
2024-07-04 04:29:12 +0100

    fix: expose a setParser method on dateAdapter class

--- demo/app/demos/date-picker/demo.component.ts
+++ demo/app/demos/date-picker/demo.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { DateAdapter } from '@vcl/ng-vcl';
+import {
+  DateAdapter,
+  DateAdapterParserDE,
+  DateAdapterParserEN,
+} from '@vcl/ng-vcl';
 
 @Component({
   templateUrl: 'demo.component.html',
@@ -12,11 +16,31 @@ export class DatePickerDemoComponent {
   monthAlt = new Date();
   time = new Date();
 
+  selectedLocale = 'en';
+
   datePattern = this.da.pattern('date');
   monthPattern = this.da.pattern('month');
   timePattern = this.da.pattern('time');
 
   onChange(date) {
     console.log('onChange', date);
+  }
+
+  toGerman() {
+    this.da.setParser(new DateAdapterParserDE(), 'de-DE');
+    this.date = new Date(this.date);
+    this.month = new Date(this.month);
+    this.monthAlt = new Date(this.monthAlt);
+    this.time = new Date(this.time);
+    this.selectedLocale = 'de';
+  }
+
+  toEnglish() {
+    this.da.setParser(new DateAdapterParserEN(), 'en-US');
+    this.date = new Date(this.date);
+    this.month = new Date(this.month);
+    this.monthAlt = new Date(this.monthAlt);
+    this.time = new Date(this.time);
+    this.selectedLocale = 'en';
   }
 }
