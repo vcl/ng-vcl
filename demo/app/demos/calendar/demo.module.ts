61068d20a5d097a5569919a4f868a0c8b3d0e10b
Bello Babakolo <fleetbeekay@gmail.com>
2025-04-25 15:37:46 +0100

    chore(calendar): remove unused variable

--- demo/app/demos/calendar/demo.module.ts
+++ demo/app/demos/calendar/demo.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { VCLCalendarModule, VCLButtonModule } from '@vcl/ng-vcl';
-import { DemoModule, DemoComponent } from './../../modules/demo/demo.module';
+import { DemoComponent } from './../../modules/demo/demo.module';
 import { CalendarDemoComponent } from './demo.component';
 import README from '!raw-loader!@vcl/ng-vcl/calendar/README.md';
 
