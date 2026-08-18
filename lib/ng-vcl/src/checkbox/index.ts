0759d47abed7aafae2bab3d5890fd3b1edda61ff
Bello Babakolo <fleetbeekay@gmail.com>
2026-08-18 16:53:53 +0100

    chore(checkbox): add backward compactible module

--- lib/ng-vcl/src/checkbox/index.ts
+++ lib/ng-vcl/src/checkbox/index.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { VCLCheckboxComponent } from './checkbox.component';
-// import { CheckboxInputGroupComponent } from './checkbox-input-group.component';
-// import { CheckboxLabelDirective } from './checkbox-label.directive';
 
 export { VCLCheckboxComponent };
+
+@NgModule({
+  imports: [VCLCheckboxComponent],
+  exports: [VCLCheckboxComponent],
+})
+export class VCLCheckboxModule {}
